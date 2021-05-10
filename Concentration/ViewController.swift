066c33b80908c13cb6eb4a80fb2cfdc1be15df5d
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-05-10 18:45:49 -0300

    implementa a ação de virar um card

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConcentrationViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    @IBAction func clickCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    
 }
 
